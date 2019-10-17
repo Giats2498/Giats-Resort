5e399495b9474693de51c179c8f90b779a61d1d9
unknown <vaggelisgia@hotmai.com>
2019-10-17 05:26:50 +0300

    make it better

--- server.js
+++ server.js
@@ -40,12 +40,18 @@ app.use('/reservation',reservation);
 
 
 
-//production mode
-if(process.env.NODE_ENV === 'production') { 
-   app.use(express.static('client/build'));  
-    app.get('*', (req, res) => {   
-     res.sendfile(path.resolve(__dirname,'client','build','index.html'));  
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 }
 
 app.listen(port, () => {
