7ec66c6114ba68628c88b68098e4451995ab0596
unknown <vaggelisgia@hotmai.com>
2019-10-17 20:18:40 +0300

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -18,11 +18,11 @@ function App() {
   <> 
     <Navbar/>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms" component={Rooms}/>
-      <Route exact path="/rooms/:slug" component={SingleRoom}/>
-      <Route exact path="/contactus" component={ContactUs} />
-      <ProtectedRoute exact path="/reservation" component={Reservation}/>
+      <Route exact path="./" component={Home} />
+      <Route exact path="./rooms" component={Rooms}/>
+      <Route exact path="./rooms/:slug" component={SingleRoom}/>
+      <Route exact path="./contactus" component={ContactUs} />
+      <ProtectedRoute exact path="./reservation" component={Reservation}/>
       <Route component={Error}/>
     </Switch>
     <Footer/>
