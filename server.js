aace9a50f9db9177158e980d54e251e35661e210
unknown <vaggelisgia@hotmai.com>
2019-10-18 03:42:50 +0300

    make it better

--- server.js
+++ server.js
@@ -39,10 +39,9 @@ app.use('/reservation',reservation);
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.use(express.static(path.join(__dirname,'client', 'build')));
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
